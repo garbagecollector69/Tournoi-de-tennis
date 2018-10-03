02e893e846506821359eef3fd6d7dc7368b0e3ae
Unknown <noragepetit@hotmail.com>
2018-10-03 14:40:36 -0400

    Declaration propriete + methode classe CPartie + constructeur initialisé

--- SimulateurDeTournoiTennis/SimulateurTournoiDeTennis/SimulateurTournoiDeTennis/CPartie.cpp
+++ SimulateurDeTournoiTennis/SimulateurTournoiDeTennis/SimulateurTournoiDeTennis/CPartie.cpp
@@ -1,3 +1,16 @@
 #include "CPartie.h"
 
-
+CPartie::CPartie(CJoueur & leJoueur1, CJoueur & leJoueur2) : m_plesJoueurs(2), m_iLesPointsJ1(0), m_iLesPointsJ2(0)
+{
+	m_plesJoueurs[0] = &leJoueur1;
+	m_plesJoueurs[1] = &leJoueur2;
+	
+	if (leJoueur1.GetSexe == Femme && leJoueur2.GetSexe == Femme)
+	{
+		m_iNbManches = 3;
+	}
+	else
+	{
+		m_iNbManches = 5;
+	}
+}
--- SimulateurDeTournoiTennis/SimulateurTournoiDeTennis/SimulateurTournoiDeTennis/CPartie.h
+++ SimulateurDeTournoiTennis/SimulateurTournoiDeTennis/SimulateurTournoiDeTennis/CPartie.h
@@ -2,14 +2,22 @@
 #define CPARTIE_H
 
 #include <vector>
+#include "CJoueur.h"
 
 class CPartie
 {
-
 public:
+	CPartie(CJoueur & leJoueur1, CJoueur & leJoueur2);
+	CJoueur* GetGagnant();
+	CJoueur* GetPerdant();
+	const string & GetResults() const;
+	void Jouer();
 
 private:
-	
+	vector<CJoueur*> m_plesJoueurs;
+	int m_iLesPointsJ1;
+	int m_iLesPointsJ2;
+	int m_iNbManches;
 };
 
 #endif
