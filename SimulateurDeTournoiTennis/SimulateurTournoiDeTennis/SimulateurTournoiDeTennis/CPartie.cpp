c535313976f82cc571ff00ccb82e6f5ad3416113
lor_louis <louis.sven@gmail.com>
2018-10-04 12:26:14 -0400

    J'ai corrigé getSexe vers getSexe()

--- SimulateurDeTournoiTennis/SimulateurTournoiDeTennis/SimulateurTournoiDeTennis/CPartie.cpp
+++ SimulateurDeTournoiTennis/SimulateurTournoiDeTennis/SimulateurTournoiDeTennis/CPartie.cpp
@@ -5,7 +5,7 @@ CPartie::CPartie(CJoueur & leJoueur1, CJoueur & leJoueur2) : m_plesJoueurs(2), m
 	m_plesJoueurs[0] = &leJoueur1;
 	m_plesJoueurs[1] = &leJoueur2;
 	
-	if (leJoueur1.GetSexe == Femme && leJoueur2.GetSexe == Femme)
+	if (leJoueur1.GetSexe() == Femme && leJoueur2.GetSexe() == Femme)
 	{
 		m_iNbManches = 3;
 	}
